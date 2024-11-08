06d0058a1fb45f37c41b0215b64c6eccce6e2499
Junnie2412 <dungchan321@gmail.com>
2024-11-09 01:05:07 +0700

    update

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -11,6 +11,13 @@ const firebaseConfig = {
   messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
   appId: Config.FIREBASE_APP_ID,
   measurementId: Config.FIREBASE_MEASUREMENT_ID
+  // apiKey: 'AIzaSyCEhnkolnR141DeAKC-l1JHpFVWgG-PaeY',
+  // authDomain: 'deliveroo-dab94.firebaseapp.com',
+  // projectId: 'deliveroo-dab94',
+  // storageBucket: 'deliveroo-dab94.appspot.com',
+  // messagingSenderId: '207678626978',
+  // appId: '1:207678626978:web:0e1cd9178b2e350846cbfb',
+  // measurementId: Config.FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
